413257163ec1bc7480082f911ed0384b78e08e62
Andres P. Torres <andres.perez.torres@gmail.com>
2019-03-16 01:24:52 +0000

    New model, updated results
    Feat: New model, updated results
    
    New model uploaded, with its results updated.
    Performance increase: not that clear in terms of
    metrics (loss, perplexity) but absolutely measurable
    in terms of language generation (no words repeated,
    coherence in text greatly improved).

--- Image_Captioning/model.py
+++ Image_Captioning/model.py
@@ -1,10 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-
 import torch.nn.functional as F
-import numpy as np
-import math
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -27,171 +24,90 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         ''' Initialize the layers of this model.'''
         super().__init__()
-    
-        # Keep track of hidden_size for initialization of hidden state
+        
         self.hidden_size = hidden_size
-        
-        # Embedding layer that turns words into a vector of a specified size
+
+        # embedding layer that turns words into a vector of a specified size
         self.word_embeddings = nn.Embedding(vocab_size, embed_size)
-        
-        # The LSTM takes embedded word vectors (of a specified size) as input
+
+        # the LSTM takes embedded word vectors (of a specified size) as inputs 
         # and outputs hidden states of size hidden_dim
-        self.lstm = nn.LSTM(input_size=embed_size, \
-                            hidden_size=hidden_size, # LSTM hidden units 
-                            num_layers=1, # number of LSTM layer
-                            bias=True, # use bias weights b_ih and b_hh
-                            batch_first=True,  # input & output will have batch size as 1st dimension
-                            dropout=0, # Not applying dropout 
-                            bidirectional=False, # unidirectional LSTM
-                           )
+        self.num_layers = num_layers
         
-        # The linear layer that maps the hidden state output dimension
-        # to the number of words we want as output, vocab_size
-        self.linear = nn.Linear(hidden_size, vocab_size)                     
+        # I never got this to work with batch_first = True
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
 
-        # initialize the hidden state
-        # self.hidden = self.init_hidden()
-        
+        # the linear layer that maps the hidden state output dimension 
+        # to the number of tags we want as output, tagset_size (in this case this is 3 tags)
+        self.hidden2tag = nn.Linear(hidden_size, vocab_size)
+
     def init_hidden(self, batch_size):
-        """ At the start of training, we need to initialize a hidden state;
-        there will be none because the hidden state is formed based on previously seen data.
-        So, this function defines a hidden state with all zeroes
-        The axes semantics are (num_layers, batch_size, hidden_dim)
-        """
-        return (torch.zeros((1, batch_size, self.hidden_size), device=device), \
-                torch.zeros((1, batch_size, self.hidden_size), device=device))
-
+        ''' At the start of training, we need to initialize a hidden state;
+           there will be none because the hidden state is formed based on perviously seen data.
+           So, this function defines a hidden state with all zeroes and of a specified size.'''
+        # The axes dimensions are (n_layers, batch_size, hidden_dim)
+        return (torch.zeros((self.num_layers, batch_size, self.hidden_size), device=device),
+                torch.zeros((self.num_layers, batch_size, self.hidden_size), device=device))
+    
     def forward(self, features, captions):
-        """ Define the feedforward behavior of the model """
+        ''' Define the feedforward behavior of the model.'''
         
-        # Discard the <end> word to avoid predicting when <end> is the input of the RNN
-        captions = captions[:, :-1]     
+        batch_size = features.shape[0]
         
-        # Initialize the hidden state
-        batch_size = features.shape[0] # features is of shape (batch_size, embed_size)
-        self.hidden = self.init_hidden(self.batch_size) 
-                
-        # Create embedded word vectors for each word in the captions
-        embeddings = self.word_embeddings(captions) # embeddings new shape : (batch_size, captions length - 1, embed_size)
+        # create embedded vectors for the image and each word in the caption
+        captions = captions[:, :-1]
+        embeds = self.word_embeddings(captions)
         
-        # Stack the features and captions
-        embeddings = torch.cat((features.unsqueeze(1), embeddings), dim=1) # embeddings new shape : (batch_size, caption length, embed_size)
+        #join_embeds_image_text = torch.cat((features.view(batch_size, 1, features.shape[1]), embeds), 1)
+        features = features.unsqueeze(1)
+        join_image_text = torch.cat((features, embeds), 1)
         
-        # Get the output and hidden state by passing the lstm over our word embeddings
+        self.hidden = self.init_hidden(batch_size)
+        
+        # get the output and hidden state by passing the lstm over our word embeddings
         # the lstm takes in our embeddings and hidden state
-        lstm_out, self.hidden = self.lstm(embeddings, self.hidden) # lstm_out shape : (batch_size, caption length, hidden_size)
+        lstm_out, _ = self.lstm(join_image_text, self.hidden)
+        
+        # get the scores for the most likely tag for a word
+        #tag_outputs = self.hidden2tag(lstm_out.view(join_embeds_image_text.shape[1], -1))
+        tag_outputs = self.hidden2tag(lstm_out)
+        
+        #tag_scores = F.log_softmax(tag_outputs, dim = 2)
+        
+        return tag_outputs
 
-        # Fully connected layer
-        outputs = self.linear(lstm_out) # outputs shape : (batch_size, caption length, vocab_size)
-
-        return outputs
-
-    ## Greedy search 
-    def sample(self, inputs):
+    def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
         
+        #current_embeds = inputs.unsqueeze(1)
+        current_embeds = inputs
         
         output = []
-        batch_size = inputs.shape[0] # batch_size is 1 at inference, inputs shape : (1, 1, embed_size)
-        hidden = self.init_hidden(batch_size) # Get initial hidden state of the LSTM
-    
-        while True:
-            lstm_out, hidden = self.lstm(inputs, hidden) # lstm_out shape : (1, 1, hidden_size)
-            outputs = self.linear(lstm_out)  # outputs shape : (1, 1, vocab_size)
-            outputs = outputs.squeeze(1) # outputs shape : (1, vocab_size)
-            _, max_indice = torch.max(outputs, dim=1) # predict the most likely next word, max_indice shape : (1)
+        batch_size = inputs.shape[0]
+        
+        if not states:
+            hidden = self.init_hidden(batch_size)
+        else:
+            hidden = states
             
-            output.append(max_indice.cpu().numpy()[0].item()) # storing the word predicted
+        for word_idx in range(0, max_len):
+            lstm_out, hidden = self.lstm(current_embeds, hidden)
+            tag_outputs = self.hidden2tag(lstm_out)
+            tag_outputs = tag_outputs.squeeze(1)
+            next_word = torch.argmax(tag_outputs, dim = 1)
             
-            if (max_indice == 1):
-                # We predicted the <end> word, so there is no further prediction to do
+            output.append(next_word.cpu().numpy()[0].item())
+            
+            if (next_word == 1):
+                #<end> word, do not continue predicting
                 break
             
-            ## Prepare to embed the last predicted word to be the new input of the lstm
-            inputs = self.word_embeddings(max_indice) # inputs shape : (1, embed_size)
-            inputs = inputs.unsqueeze(1) # inputs shape : (1, 1, embed_size)
+            current_embeds = self.word_embeddings(next_word)
+            current_embeds = current_embeds.unsqueeze(1)
             
         return output
-
-
-
-
-    ## Beam search implementation (Attempt)
-    def beam_search_sample(self, inputs, beam=3):
-        output = []
-        batch_size = inputs.shape[0] # batch_size is 1 at inference, inputs shape : (1, 1, embed_size)
-        hidden = self.init_hidden(batch_size) # Get initial hidden state of the LSTM
-        
-        # sequences[0][0] : index of start word
-        # sequences[0][1] : probability of the word predicted
-        # sequences[0][2] : hidden state related of the last word
-        sequences = [[[torch.Tensor([0])], 1.0, hidden]]
-        max_len = 20
-
-        ## Step 1
-        # Predict the first word <start>
-        outputs, hidden = DecoderRNN.get_outputs(self, inputs, hidden)
-        _, max_indice = torch.max(outputs, dim=1) # predict the most likely next word, max_indice shape : (1)
-        output.append(max_indice.cpu().numpy()[0].item()) # storing the word predicted 
-        # inputs = DecoderRNN.get_next_word_input(self, max_indice)
-        
-        
-        l = 0
-        while len(sequences[0][0]) < max_len: 
-            print("l:", l)
-            l+= 1
-            temp = []
-            for seq in sequences:
-#                 print("seq[0]: ", seq[0])
-                inputs = seq[0][-1] # last word index in seq
-                inputs = inputs.type(torch.cuda.LongTensor)
-                print("inputs : ", inputs)
-                # Embed the input word
-                inputs = self.word_embeddings(inputs) # inputs shape : (1, embed_size)
-                inputs = inputs.unsqueeze(1) # inputs shape : (1, 1, embed_size) 
-                
-                # retrieve the hidden state
-                hidden = seq[2]
-                
-                preds, hidden = DecoderRNN.get_outputs(self, inputs, hidden)
-
-                # Getting the top <beam_index>(n) predictions
-                softmax_score = F.log_softmax(outputs, dim=1) # Define a function to sort the cumulative score
-                sorted_score, indices = torch.sort(-softmax_score, dim=1)
-                word_preds = indices[0][:beam]
-                best_scores = sorted_score[0][:beam]
-
-                # Creating a new list so as to put them via the model again
-                for i, w in enumerate(word_preds):
-#                     print("seq[0]: ", seq[0][0][:].cpu().numpy().item())
-                    next_cap, prob = seq[0][0].cpu().numpy().tolist(), seq[1]
-                    
-                    next_cap.append(w)
-                    print("next_cap : ", next_cap)
-                    prob *best_scores[i].cpu().item()
-                    temp.append([next_cap, prob])
-
-            sequences = temp
-            # Order according to proba
-            ordered = sorted(sequences, key=lambda tup: tup[1])
-
-            # Getting the top words
-            sequences = ordered[:beam]
-            print("sequences: ", sequences)
-
-    def get_outputs(self, inputs, hidden):
-        lstm_out, hidden = self.lstm(inputs, hidden) # lstm_out shape : (1, 1, hidden_size)
-        outputs = self.linear(lstm_out)  # outputs shape : (1, 1, vocab_size)
-        outputs = outputs.squeeze(1) # outputs shape : (1, vocab_size)
-
-        return outputs, hidden
-
-    def get_next_word_input(self, max_indice):
-        ## Prepare to embed the last predicted word to be the new input of the lstm
-        inputs = self.word_embeddings(max_indice) # inputs shape : (1, embed_size)
-        inputs = inputs.unsqueeze(1) # inputs shape : (1, 1, embed_size)
-
-        return inputs
+            
+            
